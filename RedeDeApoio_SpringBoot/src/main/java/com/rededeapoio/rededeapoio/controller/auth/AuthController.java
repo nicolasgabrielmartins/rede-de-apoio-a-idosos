9aa5761f3631c8f7bb442b38ef4dbda5aa32ab51
Nicolas Gabriel <nicolasgabrielma@gmail.com>
2025-10-17 14:29:20 -0300

    Cria o Authentication Response

--- RedeDeApoio_SpringBoot/src/main/java/com/rededeapoio/rededeapoio/controller/auth/AuthController.java
+++ RedeDeApoio_SpringBoot/src/main/java/com/rededeapoio/rededeapoio/controller/auth/AuthController.java
@@ -2,10 +2,14 @@ package com.rededeapoio.rededeapoio.controller.auth;
 
 import com.rededeapoio.rededeapoio.dto.SignupRequest;
 import com.rededeapoio.rededeapoio.dto.UserDto;
+import com.rededeapoio.rededeapoio.repositories.UserRepository;
 import com.rededeapoio.rededeapoio.services.auth.AuthService;
+import com.rededeapoio.rededeapoio.services.jwt.UserService;
+import com.rededeapoio.rededeapoio.utils.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,6 +17,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+
+    private final UserRepository userRepository;
+
+    private final JwtUtil jwtUtil;
+
+    private final UserService userService;
+
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/signup")
     public ResponseEntity<?> signupUser (@RequestBody SignupRequest signupRequest){
@@ -23,4 +35,7 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User not created");
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUserDto);
     }
+
+
+
 }
