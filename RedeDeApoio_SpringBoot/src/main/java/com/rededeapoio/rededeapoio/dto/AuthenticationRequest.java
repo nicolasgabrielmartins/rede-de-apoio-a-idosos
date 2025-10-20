9dd457833a803dfd8db668f06f401ebdc28ddaf4
Nicolas Gabriel Martins <nicolasgabrielmartinssilva@gmail.com>
2025-10-20 15:58:34 -0300

    Cria uma api para login com Springboot

---
+++ RedeDeApoio_SpringBoot/src/main/java/com/rededeapoio/rededeapoio/dto/AuthenticationRequest.java
@@ -0,0 +1,17 @@
+package com.rededeapoio.rededeapoio.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Object getPassword() {
+        return password;
+    }
+}
---
+++ RedeDeApoio_SpringBoot/src/main/java/com/rededeapoio/rededeapoio/dto/AuthenticationResponse.java
@@ -0,0 +1,11 @@
+package com.rededeapoio.rededeapoio.dto;
+
+import com.rededeapoio.rededeapoio.enums.UserRole;
+import lombok.Data;
+
+@Data
+public class AuthenticationResponse {
+    private String jwt;
+    private Long userId;
+    private UserRole userRole;
+}
